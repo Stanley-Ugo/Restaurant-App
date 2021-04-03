07dca87ab0aea94f41032a4d9da9d573b4871dc0
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-03 16:26:19 +0100

    Adding the OrderViewModel class

---
+++ ViewModel/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantApp.ViewModel
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public int PaymemtTypeId { get; set; }
+        public int CustomerId { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
