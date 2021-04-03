7c95ce64eaf79539d33a0f89cea8eaeababdb449
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-03 18:37:05 +0100

    Adding the AddOrder method in the repository

---
+++ Repositories/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using RestaurantApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantApp.Repositories
+{
+    public class OrderRepository
+    {
+        private RestaurantDBEntities objRestaurantDBEntities;
+        public OrderRepository()
+        {
+            objRestaurantDBEntities = new RestaurantDBEntities();
+        }
+    }
+}
